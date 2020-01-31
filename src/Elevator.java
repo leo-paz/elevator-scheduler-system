93f21dd2898edac5cac577b575fe91ad3b672fb4
Mounica Pillarisetty <35469706+mounicap6@users.noreply.github.com>
2020-01-30 20:53:17 -0500

    Update Elevator.java

--- src/Elevator.java
+++ src/Elevator.java
@@ -6,8 +6,7 @@
 public class Elevator implements Runnable {
 	Scheduler s;
 	
-	public Elevator(Scheduler s)
-	{
+	public Elevator(Scheduler s) {
 		this.s = s;
 	}
 
@@ -15,15 +14,12 @@ public class Elevator implements Runnable {
 	public void run() {
 		// TODO Auto-generated method stub
 		
-		while(true)
-		{
-			FloorButtonRequest a = s.getElevator();
-			System.out.println(Thread.currentThread().getName() + " has been requested " + " and elevator goes " + a.getDirection().toString() + " to Floor: " + a.getFloorNum());
-			System.out.println("User gets on the elevator and the elevator goes to Floor " + a.getDestinationFloor());
+		while(true) {
+			FloorButtonRequest request = s.getElevator();
+			System.out.println(Thread.currentThread().getName() + " has been requested and elevator goes " + request.getDirection().toString() + " to floor " + request.getFloorNum() + " for pickup.");
+			System.out.println("The passenger gets on the elevator and goes to Floor " + request.getDestinationFloor());
 		}
 		
 	}
-
-	
 	
 }
