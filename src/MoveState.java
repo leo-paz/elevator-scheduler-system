172d8b9b9180c3f72a0dbb7e6077d11860d3a694
Sarah Lamonica <sarahlamonica444@gmail.com>
2020-02-14 11:11:48 -0500

    added code for moving state to the code

--- src/MoveState.java
+++ src/MoveState.java
@@ -9,12 +9,21 @@ public class MoveState extends State {
 	@Override
 	public void moveDoor() {
 		// TODO Auto-generated method stub
-		
+		System.out.println("Doors are closed while the elevator is moving");
 	}
 
 	@Override
 	public void moveElevator() {
 		// TODO Auto-generated method stub
+		elevator.setElevatorMoving();
+		System.out.println("Elevator state: Moving");
+		
+		try {
+			Thread.sleep(FloorToFloorTime);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
